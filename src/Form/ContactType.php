841b0abe47f442aa1cc93ed5364ee2006bb8bc5c
Adrien-Gardien <gardienadrien@gmail.com>
2023-05-16 10:47:22 +0200

    Préparation Examen blanc (25% du site fait)

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -16,18 +16,37 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                'attr' => ['class' => 'tinymce'],
-            ])
-
-            ->add('content', TextareaType::class, [
-                'attr' => ['class' => 'tinymce'],
-            ])
- 
-            ->add('submit', SubmitType::class, [
-                'attr' => ['class' => 'btn btn-danger btn-lg'],
-            ])
-        ;
+        ->add('email', EmailType::class, [
+            'attr' => [
+                'class' => 'form-control tinymce my-3 bold',
+                'placeholder' => 'Email',
+            ],
+            'label' => false,
+            'required' => false,
+            'row_attr' => [
+                'id' => 'email-input-div', // Renommer l'ID du div
+                'class' => 'email-input-div', // Renommer la classe du div
+            ],
+        ])
+        ->add('content', TextareaType::class, [
+            'attr' => [
+                'class' => 'form-control tinymce my-3 bold',
+                'placeholder' => 'Votre message',
+            ],
+            'label' => false,
+            'required' => false,
+            'row_attr' => [
+                'id' => 'content-input-div', // Renommer l'ID du div
+                'class' => 'content-input-div', // Renommer la classe du div
+            ],
+        ])
+        ->add('Envoyer', SubmitType::class, [
+            'attr' => ['class' => 'btn btn-danger btn-lg'],
+            'row_attr' => [
+                'id' => 'submit-button-div', // Renommer l'ID du div
+                'class' => 'submit-button-div', // Renommer la classe du div
+            ],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
